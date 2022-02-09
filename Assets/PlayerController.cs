4bc6d803861ea17c8ffff046c45cbf3746dbd479
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-02-09 13:06:44 +0100

    Added some stuff

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  void Start()
-  {
 
-  }
+  public float speed = 9001f;
 
   void Update()
   {
     float xMovement = Input.GetAxisRaw("Horizontal"); // -1 till +1
+    float yMovement = Input.GetAxisRaw("Vertical"); // -1 till +1
 
-    Vector2 movement = new Vector2(xMovement, 0) * 0.02f;
+    Vector2 horizontalMovement = new Vector2(xMovement, 0) * speed;
+    Vector2 verticalMovement = new Vector2(0, yMovement) * speed;
 
-    transform.Translate(movement);
+    transform.Translate(horizontalMovement + verticalMovement);
+
+    if (Mathf.Abs(transform.position.x) > 5)
+    {
+      transform.Translate(-horizontalMovement);
+    }
 
   }
 }
